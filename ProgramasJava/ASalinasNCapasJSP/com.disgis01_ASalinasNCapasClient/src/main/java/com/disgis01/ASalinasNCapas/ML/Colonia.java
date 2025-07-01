c48246162c5a0bdaa0e5dd099058ea56c29c00b3
Alien 1 <Alien 1@ALIEN1>
2025-07-01 15:07:42 -0600

    Inicio de Client

---
+++ ProgramasJava/ASalinasNCapasJSP/com.disgis01_ASalinasNCapasClient/src/main/java/com/disgis01/ASalinasNCapas/ML/Colonia.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.disgis01.ASalinasNCapas.ML;
+
+/**
+ *
+ * @author Alien 1
+ */
+public class Colonia {
+    private int IdColonia;
+    private String NombreColonia;
+    private String CodigoPostal;
+    public Municipio Municipio;
+
+    public int getIdColonia() {
+        return IdColonia;
+    }
+
+    public void setIdColonia(int IdColonia) {
+        this.IdColonia = IdColonia;
+    }
+
+    public String getNombreColonia() {
+        return NombreColonia;
+    }
+
+    public void setNombreColonia(String NombreColonia) {
+        this.NombreColonia = NombreColonia;
+    }
+
+    public String getCodigoPostal() {
+        return CodigoPostal;
+    }
+
+    public void setCodigoPostal(String CodigoPostal) {
+        this.CodigoPostal = CodigoPostal;
+    }
+
+    public Municipio getMunicipio() {
+        return Municipio;
+    }
+
+    public void setMunicipio(Municipio Municipio) {
+        this.Municipio = Municipio;
+    }
+    
+    
+}
